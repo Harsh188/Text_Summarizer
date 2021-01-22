d4f0ace6dbe60179ba0e985707bd63ef7f68cf26
Harsh188 <hiharshith18@gmail.com>
2021-01-22 22:45:28 +0530

    changed input to textarea

--- text-summarizer-website/src/components/Input.js
+++ text-summarizer-website/src/components/Input.js
@@ -4,9 +4,8 @@ function Input(){
 	return (
 		<div className="input">
 			<h1>INPUT</h1>
-			<label>Enter your text here: </label>
-			<input type='textarea'
-				name='textValue'
+			<label>Enter your text here:</label>
+			<textarea 
 			/>
 		</div>
 	)
