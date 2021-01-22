63059c8680986fb3e5009eee6efa366862a10bd1
Harsh188 <hiharshith18@gmail.com>
2021-01-22 22:28:12 +0530

    initial commit

---
+++ text-summarizer-website/src/app/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Router>
+      </Router>
+    </div>
+    </>
+  );
+}
+
+export default App;
