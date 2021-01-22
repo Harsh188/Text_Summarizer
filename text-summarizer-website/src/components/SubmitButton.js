fe2d7df3da7172cdb39f40e9cc16d8357ccc7ea6
Harsh188 <hiharshith18@gmail.com>
2021-01-22 23:06:42 +0530

    updated components to add submit button

--- text-summarizer-website/src/components/SubmitButton.js
+++ text-summarizer-website/src/components/SubmitButton.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
 
 function SubmitButton(){
 	return (
 		<div className="submitbutton">
 			<h1>SUBMITBUTTON</h1>
+			<Button variant="outline-primary">Submit</Button>{' '}
 		</div>
 	)
 }
--- text-summarizer-website/src/components/index.js
+++ text-summarizer-website/src/components/index.js
@@ -1,5 +1,6 @@
 import Input from './Input'
 import Output from './Output'
 import SubmitButton from './SubmitButton'
+import Button from 'react-bootstrap/Button';
 
 export { Input, Output, SubmitButton }
