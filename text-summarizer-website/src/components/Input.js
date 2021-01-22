96de145829f9253091e36008da77483f3d1d3fd2
Harsh188 <hiharshith18@gmail.com>
2021-01-22 22:41:50 +0530

    adding text area

--- text-summarizer-website/src/components/Input.js
+++ text-summarizer-website/src/components/Input.js
@@ -4,6 +4,10 @@ function Input(){
 	return (
 		<div className="input">
 			<h1>INPUT</h1>
+			<label>Enter your text here: </label>
+			<input type='textarea'
+				name='textValue'
+			/>
 		</div>
 	)
 }
