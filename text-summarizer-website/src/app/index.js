1fbcdaf5285cd66b5b38f4071fdb855a8a077aaa
Harsh188 <hiharshith18@gmail.com>
2021-01-22 22:38:09 +0530

    updated the main app index.js

--- text-summarizer-website/src/app/index.js
+++ text-summarizer-website/src/app/index.js
@@ -2,11 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { Input, Output, SubmitButton } from '../components'
+
 function App() {
   return (
     <>
     <div className="App">
       <Router>
+      	<Input />
+      	<SubmitButton />
+      	<Switch>
+      		<Route path='/summarizedText' exact component={Output}/>
+      	</Switch>
       </Router>
     </div>
     </>
