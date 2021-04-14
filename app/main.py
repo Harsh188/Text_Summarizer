4c806f5144498ecefee5817f51ee43c0f814fe5e
Harsh188 <hiharshith18@gmail.com>
2021-04-14 17:06:28 +0530

    fixed several errors and got it working locally

--- app/main.py
+++ app/main.py
@@ -4,23 +4,26 @@ from transformers import PegasusTokenizer, PegasusForConditionalGeneration
 
 app = Flask(__name__)
 
-model_path = "googe/pegasus-xsum"
+model_path = "./pegasus-xsum"
 
 @app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/summarize', methods=['GET','POST'])
 def summarize():
     text = request.args.get('text')
     api_key = request.args.get('api_key')
     if text is None or api_key != "MyCustomerApiKey":
         return jsonify(code=403, message="bad request")
 
-    tokenizer = PegasusTokenizer.from_pretrained("google/pegasus-xsum",local_files_only=True)
-    model = PegasusForConditionalGeneration.from_pretrained("google/pegasus-xsum",local_files_only=True)
+    tokenizer = PegasusTokenizer.from_pretrained(model_path,local_files_only=True,force_download=True)
+    model = PegasusForConditionalGeneration.from_pretrained(model_path,local_files_only=True,force_download=True)
     inputs = tokenizer([text],max_length=1024,return_tensors='pt')
     summary_ids = model.generate(inputs['input_ids'])
     summary = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids]
-    print(summary)
+    # print(summary)
     return jsonify(summary)
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
