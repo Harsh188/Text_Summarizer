8eeefab9008699955616cf9a47677434996e241b
Harsh188 <hiharshith18@gmail.com>
2021-05-01 07:18:58 +0530

    added a new navbar

--- text-summarizer-website/src/components/index.js
+++ text-summarizer-website/src/components/index.js
@@ -3,6 +3,7 @@ import Output from './Output'
 import SubmitButton from './SubmitButton'
 import Heading from './Heading'
 import Button from 'react-bootstrap/Button';
+import NavBar from './NavBar'
 import '../style/css/main.css'
 
-export { Input, Output, SubmitButton, Heading}
+export { Input, Output, SubmitButton, Heading, NavBar}
